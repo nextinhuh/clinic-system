5d0217b156ead7d298921e574d64848eb6c54cc0
nextinhuh <alvaroneto.dev@gmail.com>
2024-01-29 21:47:00 -0300

    feature: config eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { Label } from "@radix-ui/react-label";
-import { Button } from "./components/ui/button";
-import { Input } from "./components/ui/input";
+import { Label } from '@radix-ui/react-label'
+import { Button } from './components/ui/button'
+import { Input } from './components/ui/input'
 
 export function App() {
   return (
@@ -9,13 +9,15 @@ export function App() {
 
       <div className="flex flex-col items-center justify-center w-1/3 max-w-[700] p-6">
         <form action="" className="w-96 flex flex-col gap-3">
-        <Label>E-mail</Label>
-        <Input type="email" id="email" placeholder="E-mail"/>
+          <Label>E-mail</Label>
+          <Input type="email" id="email" placeholder="E-mail" />
 
-        <Label className="">Senha</Label>
-        <Input type="password" id="password" placeholder="Senha"/>
+          <Label className="">Senha</Label>
+          <Input type="password" id="password" placeholder="Senha" />
 
-        <Button className="mt-6" variant='outline'>Teste</Button>
+          <Button className="mt-6" variant="outline">
+            Teste
+          </Button>
         </form>
       </div>
     </div>
