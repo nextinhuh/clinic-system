437a27de32290f554d6b298617b4fb16a16d46e9
Thiago Torres de Souza <thiagomev@gmail.com>
2024-05-08 14:24:54 -0300

    feature: Add calendar in form controller

--- src/components/form-controller/index.tsx
+++ src/components/form-controller/index.tsx
@@ -17,6 +17,12 @@ import {
   SelectTrigger,
   SelectValue,
 } from '../ui/select'
+import { Button } from '../ui/button'
+import { CalendarIcon } from '@radix-ui/react-icons'
+import { format } from 'date-fns'
+import { cn } from '@/utils/tw-merge'
+import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover'
+import { Calendar } from '../ui/calendar'
 
 interface IOptions {
   id: string
@@ -32,7 +38,13 @@ interface InputListProps {
   options?: IOptions[]
 }
 
-type InputType = 'text' | 'textarea' | 'select' | 'email' | 'password'
+type InputType =
+  | 'text'
+  | 'textarea'
+  | 'select'
+  | 'email'
+  | 'password'
+  | 'calendar'
 
 interface FormControllerProps {
   form: UseFormReturn<any>
@@ -69,6 +81,38 @@ function renderInput(
           </SelectContent>
         </Select>
       )
+    case 'calendar':
+      return (
+        <Popover>
+          <PopoverTrigger asChild>
+            <FormControl>
+              <Button
+                variant={'outline'}
+                className={cn(
+                  'w-[240px] pl-3 text-left font-normal',
+                  !field.value && 'text-muted-foreground',
+                )}
+              >
+                {field.value ? (
+                  format(field.value, 'PPP')
+                ) : (
+                  <span>{placeholder}</span>
+                )}
+                <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+              </Button>
+            </FormControl>
+          </PopoverTrigger>
+          <PopoverContent className="w-auto p-0" align="start">
+            <Calendar
+              mode="single"
+              selected={field.value}
+              onSelect={field.onChange}
+              disabled={(date) => date < new Date()}
+              initialFocus
+            />
+          </PopoverContent>
+        </Popover>
+      )
     default:
       return (
         <Input type={type || 'text'} placeholder={placeholder} {...field} />
@@ -97,7 +141,7 @@ export function FormController({
                   input.formItemClassName ? input.formItemClassName : 'w-[100%]'
                 }
               >
-                <FormLabel>{input.label}</FormLabel>
+                <FormLabel className="mr-2">{input.label}</FormLabel>
                 <FormControl>
                   {renderInput(
                     input.type as InputType,
