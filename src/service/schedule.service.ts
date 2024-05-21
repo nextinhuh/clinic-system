36a4cb5916aa5ebc1566e88066a0281f81765049
Thiago Torres de Souza <thiagomev@gmail.com>
2024-05-21 20:04:21 -0300

    Add function service to get Schedules by doctorId

--- src/service/schedule.service.ts
+++ src/service/schedule.service.ts
@@ -1,5 +1,16 @@
-import { CreateScheduleAppointmentFormSchema } from '@/utils/types'
-import { addDoc, collection, getFirestore } from 'firebase/firestore'
+import { scheduleAppointmentSchema } from '@/utils/schemas'
+import {
+  CreateScheduleAppointmentFormSchema,
+  ScheduleAppointmentSchemaData,
+} from '@/utils/types'
+import {
+  addDoc,
+  collection,
+  getDocs,
+  getFirestore,
+  query,
+  where,
+} from 'firebase/firestore'
 
 const SCHEDULES_FIRESTORE_KEY = 'schedules'
 
@@ -13,4 +24,32 @@ export async function createSchedule(
       throw new Error(error.message)
     },
   )
+}
+
+export async function getSchedules(
+  doctorId: string,
+): Promise<ScheduleAppointmentSchemaData[]> {
+  const scheduleList: ScheduleAppointmentSchemaData[] = []
+  const db = getFirestore()
+
+  const q = query(
+    collection(db, SCHEDULES_FIRESTORE_KEY),
+    where('doctorId', '==', doctorId),
+  )
+
+  const querySnapshot = await getDocs(q)
+
+  querySnapshot.forEach((doc) => {
+    const schedule = scheduleAppointmentSchema.parse({
+      id: doc.id,
+      patientId: doc.data().patientId,
+      date: doc.data().date,
+      patientName: doc.data().patientName,
+      doctorId: doc.data().doctorId,
+      hasConfirm: doc.data().hasConfirm,
+      consultId: doc.data().consultId ? doc.data().consultId : '',
+    })
+    scheduleList.push(schedule)
+  })
+  return scheduleList
 }
